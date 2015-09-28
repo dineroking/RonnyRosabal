6c36bc01cc7f9363e98caa51b70965a5eafa5e27
Ronny Rosabal <ronnyrosabal@gmail.com>
2015-09-28 16:26:37 -0500

    adds initial starter files

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<?php
+  /**
+   * Project: RonnyRosabal.
+   * By: Ronny Rosabal
+   * Date: 9/28/15
+   * Time: 4:18 PM
+   * Content: contains footer code for ronnyrosabal.com
+   */
---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<?php
+  /**
+   * Project: RonnyRosabal.
+   * By: Ronny Rosabal
+   * Date: 9/28/15
+   * Time: 4:17 PM
+   * Content: contains header code for ronnyrosabal.com
+   */
