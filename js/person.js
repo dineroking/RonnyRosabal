9abd6c6c1534ef74b285acd93c32d7e60cb1bee6
Ronny Rosabal <ronnyrosabal@gmail.com>
2015-12-19 19:01:43 -0600

    updates person.js

---
+++ js/person.js
@@ -0,0 +1,17 @@
+/**
+ * Project: RonnyRosabal.
+ * By: Ronny Rosabal
+ * Date: 12/19/15
+ * Time: 5:02 PM
+ * Content: Person object
+ */
+
+function Person(firstName, lastName, age){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+}
+
+Person.prototype.isLegal = function(){
+  return this.age >= 18;
+};
