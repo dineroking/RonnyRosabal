56f7f781840ca78eb8b9d35798327efb76d16518
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-01-11 11:49:00 -0600

    initializes the waitingRoom challenge

---
+++ challenges/js/waitingRoom/waitingRoom.js
@@ -0,0 +1,10 @@
+/**
+ * Project: RonnyRosabal.
+ * By: Ronny Rosabal
+ * Date: 1/2/16
+ * Time: 5:31 PM
+ * Content:
+ */
+
+//take a input which represents a number of seats
+//create all the seats
