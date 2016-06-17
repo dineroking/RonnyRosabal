1634e2a11cd1af3cfe82c21e9ea580a54077da84
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-06-16 20:42:16 -0500

    adds comments for Sort By Example challenge

--- challenges/js/sortByExample/sortByExample.js
+++ challenges/js/sortByExample/sortByExample.js
@@ -3,5 +3,19 @@
  * By: Ronny Rosabal
  * Date: 6/16/16
  * Time: 2:11 PM
- * Content:
+ * Content: CodeWars challenge "Sort By Example"
+ */
+
+/*
+*Given an Array and an Example-Array to sort to, write a function that sorts the Array following the Example-Array.
+
+ Assume Example Array catalogs all elements possibly seen in the input Array. However, the input Array does not necessarily have to have all elements seen in the Example.
+
+ Example:
+
+ Arr: [1,3,4,4,4,4,5]
+
+ Example Arr: [4,1,2,3,5]
+
+ Result: [4,4,4,4,1,3,5]
  */
