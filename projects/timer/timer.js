7df548158294b69d8894a02b3ee5e106ebaae00b
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-07-01 09:52:19 -0500

    creates navigation menu

--- projects/timer/timer.js
+++ projects/timer/timer.js
@@ -3,5 +3,21 @@
  * By: Ronny Rosabal
  * Date: 6/26/16
  * Time: 1:13 PM
- * Content:
+ * Content: main javascript for the timer project
  */
+
+var activeNav;
+var today;
+var timeHolder = $("#timeHolder");
+
+var navToggler = function(){
+  $("a").removeClass("active");
+  $(this).addClass("active");
+};
+
+$("a").on("click", navToggler);
+
+setInterval(function(){
+  today = new Date();
+  timeHolder.html(today);
+}, 500 );
