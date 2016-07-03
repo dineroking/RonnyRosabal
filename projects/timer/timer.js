c6de68d8081e238d1bdfe61808fd0f5cfd7a43fe
Ronny Rosabal <ronnyrosabal@gmail.com>
2016-07-03 11:46:29 -0500

    gets the date timer working

--- projects/timer/timer.js
+++ projects/timer/timer.js
@@ -6,18 +6,85 @@
  * Content: main javascript for the timer project
  */
 
-var activeNav;
-var today;
-var timeHolder = $("#timeHolder");
+$( window ).load( function(){
+  var activeNavId, today, dateTimer, daysLeft, hoursLeft, minutesLeft, secondsLeft,  timeHolder = $( "#timeHolder" );
 
-var navToggler = function(){
-  $("a").removeClass("active");
-  $(this).addClass("active");
-};
+  //NAV MENU
 
-$("a").on("click", navToggler);
+  var getActiveNav = function(){
+    var anchors = document.getElementsByClassName("active");
+    activeNavId = $(anchors[0]).attr("id");
+  };
 
-setInterval(function(){
-  today = new Date();
-  timeHolder.html(today);
-}, 500 );
+  var navToggler = function( elem ){
+    $( "a" ).removeClass( "active" );
+    $( elem ).addClass( "active" );
+  };
+
+  $( "a" ).on( "click", function(){
+    navToggler( this );
+    getActiveNav();
+    formVisibility();
+  } );
+
+
+  //ALL FORMS
+
+  var formVisibility = function(){
+    var formId = "#" + activeNavId + "_form";
+    $("form").css("display", "none");
+    $(formId).css("display", "block");
+  };
+
+  //DATE FORM
+
+  var getDateTimer = function() {
+    dateTimer = $("#date_input").val();
+  };
+
+  var getTimeLeft = function(){
+    var targetDate = new Date( dateTimer );
+    targetDate.setHours( targetDate.getHours() + 5 );// makes correction of time
+    targetDate = targetDate.getTime();
+    today = new Date().getTime();
+    var timeLeft = targetDate - today;
+    daysLeft = Math.floor( timeLeft / ( 1000 * 60 * 60 * 24 ) );
+    hoursLeft = Math.floor( timeLeft / ( 1000 * 60 * 60 ) % 24  );
+    minutesLeft = Math.floor( timeLeft / (1000 * 60 ) % 60 );
+    secondsLeft = Math.floor( timeLeft / ( 1000 ) % 60 );
+    return daysLeft + "d . " + hoursLeft + "h . " +  minutesLeft + "m . " + secondsLeft + "s";
+  };
+
+  $("#date_submit").on("click", function( event ){
+    event.preventDefault();
+    getDateTimer();
+    getTimeLeft();
+    testFunction();
+  } );
+
+  var testFunction = function(){
+    setInterval( function(){
+      var timeLeft = getTimeLeft();
+      timeHolder.html( timeLeft );
+    }, 500);
+  };
+
+  
+if( activeNavId === "clock" ){
+
+  setInterval( function(){
+    today = new Date();
+    timeHolder.html( today );
+  }, 500 );
+  
+}
+
+  //FUNCTION CALLS WHEN DOCUMENT LOADS
+
+  getActiveNav();
+  getDateTimer();
+
+} );
+
+
+
